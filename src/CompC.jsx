4d2e8aa8b5b7ab182618c658e0ecd805fb02159a
achal6076 <Achal6076@gmail.com>
2023-03-02 17:18:06 +0530

    use context

--- src/CompC.jsx
+++ src/CompC.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import CompD from "./CompD";
+import { FirstName,LastName } from "./App";
 
 const CompC =()=>{
-    return <CompD/>
+    const firstname = useContext(FirstName);
+    const lasttname = useContext(LastName);
+    
+    return <h1>my name is {lasttname} {firstname}</h1>
+
+    // return <CompD/>
 }
 
 export default CompC;
